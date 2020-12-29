0806a300a8a6d34d480078e88da81e831adf975b
Maxence <lemairemaxence@hotmail.com>
2020-12-29 11:59:43 +0100

    exo 2 in progress

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+let prenom = 'Maxence'
+alert(prenom.charAt(0));
+alert(prenom.slice(-1));
+alert(prenom.slice(1));
+
+prompt((prenom.charAt(1)).toUppercase)
+
+
+// alert(prenom.charAt(1).toUppercase)
