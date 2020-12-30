b8993bda5e4612f3aa00651c8878019e0413adf0
Maxence <lemairemaxence@hotmail.com>
2020-12-30 09:39:36 +0100

    correction

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,47 @@
 let prenom = 'Maxence'
 // alert(prenom.charAt(0));
+
+//2 
+
 // alert(prenom.slice(-1));
+// alert(prenom.charAt(prenom.length -1))
+
+// 3
+
 // alert(prenom.slice(1));
+// alert(prenom.substring(1))
+
+// 4
 
 // let prenom = prompt("Prénom en minuscule svp :")
 // alert(`${prenom.substr(0, 1)}${prenom.charAt(1).toUpperCase()}${prenom.substr(2)}`);
 
-// let letMaj = prompt("Quelle lettre désirez vous mettre en majuscule?")
+// prenom = prenom.toLocaleLowerCase();
+// alert(prenom.charAt(0) + prenom.charAt(1).toLocaleUpperCase() + prenom.slice(2));
+
+// 5
+
+// let letMaj = prompt("Quelle lettre désirez vous mettre en majuscule?").toLocaleLowerCase();
 // alert(prenom.replace(letMaj, letMaj.toUpperCase()));
 
+// 6
+
 // let prenomClean = prompt("Prénom ?")
-// alert(`${prenom.charAt(0).toUpperCase()}${prenom.substr(1, 6)}`)
+// alert(`${prenom.charAt(0).toUpperCase()}${prenom.substring(1)}`)
 
 // alert(prenom.charAt(1).toUppercase)
 
-for(var i = 0; i < 1; i++) {
-    prenom = removeRandomLetter(prenom);
-}
-alert(prenom);
+// 7
 
-function removeRandomLetter(prenom) {
-    var pos = Math.floor(Math.random()*prenom.length);
-    return prenom.substring(0, pos)+prenom.substring(pos+1);
-}
+// for(var i = 0; i < 1; i++) {
+//     prenom = removeRandomLetter(prenom);
+// }
+// alert(prenom);
+
+// function removeRandomLetter(prenom) {
+//     var pos = Math.floor(Math.random()*prenom.length);
+//     return prenom.substring(0, pos)+prenom.substring(pos+1);
+// }
 
 // .toString(0).substring(0)
 
@@ -31,3 +50,9 @@ function removeRandomLetter(prenom) {
 // let pos = Math.floor(Math.random()*prenom.length);
 // nomRand = prenom.substr(0, pos)+prenom.substr(pos+1);
 // alert(nomRand);
+
+// AYHAN
+
+let nbAl = Math.round(Math.random()* prenom.length)
+console.log(nbAl);
+alert(prenom.replace(prenom.charAt(nbAl), ""))
